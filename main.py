fd47599191f2490b348b59f85420e774e8d253c7
Andrej <pytpoint@gmail.com>
2026-02-13 21:07:17 +0100

    task

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-print("goit-core-hw")
-print("goit-core-hw")
-print("goit-core-hw")
-print("goit-core-hw")
-print("111111111111")
+class Human:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"Human named {self.name} who is {self.age} years old"
+    
+    def __repr__(self):
+        return f"Human({self.name}, {self.age})"
+
+human = Human("Alice", 30)
+print(human)
