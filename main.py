7f61bee43762fbe9b86556d0474dfa04563eb939
Andrej <pytpoint@gmail.com>
2026-02-15 21:31:39 +0100

    asdf

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-class Human:
-    def __init__(self, name: str, age: int):
-        self.name = name
-        self.age = age
+def count_up(n):
+    for i in range(n):
+        yield i
 
-    def __str__(self):
-        return f"Human named {self.name} who is {self.age} years old"
-    
-    def __repr__(self):
-        return f"Human({self.name}, {self.age})"
 
-human = Human("Alice", 30)
-print(human)
+g = count_up(5)
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
